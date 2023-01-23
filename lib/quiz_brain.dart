62da27d1a07b89c47485051cf7cb8e054e98319c
gartservice <gart.service@yandex.ru>
2023-01-24 06:00:02 +0800

    Add quiz brain file

---
+++ lib/quiz_brain.dart
@@ -0,0 +1,9 @@
+import 'question.dart';
+
+class QuizBrain{
+  List<Question> questionBank = [
+    Question(q: 'First question', a: false),
+    Question(q: 'Second question', a: true),
+    Question(q: 'Third question', a: true),
+  ];
+}
