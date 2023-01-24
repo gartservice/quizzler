72e63f436e172747bbd9a086078a0cee31a1bc35
gartservice <gart.service@yandex.ru>
2023-01-25 05:23:53 +0800

    Finish check was added

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -1,22 +1,46 @@
 import 'question.dart';
 
-class QuizBrain{
+class QuizBrain {
   int _questionNumber = 0;
   List<Question> _questionBank = [
     Question(q: 'First question', a: false),
     Question(q: 'Second question', a: true),
     Question(q: 'Third question', a: false),
+    Question(q: '4 question', a: false),
+    Question(q: '5 question', a: false),
+    Question(q: '6 question', a: false),
   ];
-  String getQuestionText(){
+
+  String getQuestionText() {
     return _questionBank[_questionNumber].questionText;
   }
-  bool getCorrectAnswer(){
+
+  bool getCorrectAnswer() {
     print('Number is $_questionNumber');
     return _questionBank[_questionNumber].questionAnswer;
   }
-  void nextQuestion(){
-    if(_questionNumber<_questionBank.length-1){
+
+  void nextQuestion() {
+    if (_questionNumber < _questionBank.length - 1) {
       _questionNumber++;
     }
+
+  }
+
+  bool isFinished() {
+    print('isFinish $_questionNumber');
+    int len = _questionBank.length - 1;
+    print('_questionBank.length $len');
+    if (_questionNumber == (_questionBank.length - 1)) {
+      reset();
+      return true;
+    } else {
+      return false;
+    }
+    // return _questionNumber == (_questionBank.length - 1)?true:false;
+  }
+  void reset(){
+    _questionNumber = 0;
+    print('reset');
   }
 }
